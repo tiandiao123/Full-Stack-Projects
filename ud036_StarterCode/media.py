8edf1bbd681b30f47e606dff7e59f24e667b0163
Cuiqing Li <lixx3527@gmail.com>
2017-11-30 20:08:18 -0500

    add contents in media.py

---
+++ ud036_StarterCode/media.py
@@ -0,0 +1,7 @@
+import os
+
+class Movie():
+	def __init__(self,movie_title,poster_url,trailer_youtube):
+		self.title = movie_title
+		self.poster_image_url = poster_url
+		self.trailer_youtube_url = trailer_youtube
