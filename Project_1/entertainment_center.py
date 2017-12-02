8d9e9eb7c65b685eca23ee813ad38f2cacdd0c07
Cuiqing Li <lixx3527@gmail.com>
2017-12-02 02:10:29 -0500

    add more files

--- Project_1/entertainment_center.py
+++ Project_1/entertainment_center.py
@@ -4,20 +4,22 @@ import media
 
 # Moon Light movie: Movie name, poster url and trailer youtube url
 moonlight = media.Movie("MOON LIGHT",
-	"https://assets.mubi.com/images/notebook/post_images/22267/images-w1400.jpg?1474980339",
-	"https://www.youtube.com/watch?v=9NJj12tJzqc")
+                        "https://assets.mubi.com/images/notebook/post_images" +
+                        "/22267/images-w1400.jpg?1474980339",
+                        "https://www.youtube.com/watch?v=9NJj12tJzqc")
 
-# Spider Man movie: Movie name,poster url and trailer youtube url
+# Spider Man movie: Movie name, poster url and trailer youtube url
 spiderman = media.Movie("Spider Man",
-	"https://www.technobuffalo.com/wp-content/uploads/2017/07/SpiderManPoster.jpg",
-	"https://www.youtube.com/watch?v=DiTECkLZ8HM")
-
+                        "https://www.technobuffalo.com/wp-content/uploads" +
+                        "/2017/07/SpiderManPoster.jpg",
+                        "https://www.youtube.com/watch?v=DiTECkLZ8HM")
 
 # Mother movie: movie name, poster url and trailer youtube url
 Mother = media.Movie("The Mother",
-	"https://i0.wp.com/teaser-trailer.com/wp-content/uploads/Mother-Movie-New-Poster-2.jpg?ssl=1",
-	"https://www.youtube.com/watch?v=XpICoc65uh0")
+                     "https://i0.wp.com/teaser-trailer.com/wp-content" +
+                     "/uploads/Mother-Movie-New-Poster-2.jpg?ssl=1",
+                     "https://www.youtube.com/watch?v=XpICoc65uh0")
 
-movies = [moonlight,spiderman,Mother]
+movies = [moonlight, spiderman, Mother]
 
 fresh_tomatoes.open_movies_page(movies)
--- Project_1/media.py
+++ Project_1/media.py
@@ -1,9 +1,10 @@
 import os
 
+
 class Movie():
-	# the constructor function which is used to initialize movie attributes
-	def __init__(self,movie_title,poster_url,trailer_youtube):
-		# moview attributes: movie name, movie poster and movie youtube url
-		self.title = movie_title
-		self.poster_image_url = poster_url
-		self.trailer_youtube_url = trailer_youtube
+    # the constructor function which is used to initialize movie attributes
+    def __init__(self, movie_title, poster_url, trailer_youtube):
+        # moview attributes: movie name, movie poster and movie youtube url
+        self.title = movie_title
+        self.poster_image_url = poster_url
+        self.trailer_youtube_url = trailer_youtube
